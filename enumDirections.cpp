cdbad69e46f7124019d697bebb83e1688b0fd357
Wali Morris <48896622+walimorris@users.noreply.github.com>
2020-12-29 14:59:42 -0800

    Add files via upload

---
+++ enumDirections.cpp
@@ -0,0 +1,19 @@
+/**
+ * Displays enum type named Direction with four directions. 
+ *
+ * By: Wali
+ */ 
+
+#include<iostream> 
+#include<string> 
+
+using namespace std; 
+
+int main() { 
+
+    enum Directions {N, S, E, W}; 
+
+    cout << W << " " << E << " " << S << " " << N << " " << endl;
+
+    return 0; 
+} 
