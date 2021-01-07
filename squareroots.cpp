27f4f990d612176d01f3fb73c8d930822e9b47d0
Wali Morris <48896622+walimorris@users.noreply.github.com>
2021-01-06 19:13:52 -0800

    Add files via upload

---
+++ squareroots.cpp
@@ -0,0 +1,22 @@
+/**
+ * Write a complete C++ program to compute and output the square 
+ * roots of the whole numbers 1 to 10. 
+ *
+ * By: Wali 
+ */ 
+
+#include<iostream> 
+#include<string> 
+#include<cstdlib> 
+#include<cmath> 
+
+using namespace std; 
+
+int main() { 
+    for(int i = 1; i < 11; i++) { 
+        double squareroot = sqrt(i); 
+	cout << "Square Root of " << i << ": "
+	     <<  squareroot << endl; 
+    } 
+    return 0; 
+} 
